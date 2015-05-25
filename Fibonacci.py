9ebbe00bbe73235fb0d62bf54c38798d3bac46cb
Dave O'Connor <DJO3@users.noreply.github.com>
2015-05-25 19:04:06 -0400

    update Fibonacci.py
    generators!

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,36 +1,19 @@
-## Creates a list of fibonacci numbers and returns nth term in fibonacci
+# Fibonacci generator
+def fib():
+    a,b = 0,1
+    while True:
+        yield a
+        a, b = b, a+b
 
-def f1(n):
-    assert type(n) == int and n >  0
-    terms = [0,1]
-    i = 2
-    while i<=n:
-        terms.append(terms[-1]+terms[-2])
-        i = i + 1
-        #print (i-1), '=', terms
-    print  "Digit #%s within Fibonacci is " %n + str(terms[-2]) 
+# num = fib()
+# for x in range(100):
+#     print(num.next())
 
-## Recursively produces corresponding nth term in fibonacci
+# Returns nth digit of Fibonacci
+def fib2(num):
+    a,b = 0,1
+    for _ in range(num-1):
+        a, b = b, a+b
+    return a
 
-def f2(n):
-    assert type(n) == int and n >  0
-    if n <= 1:
-        print "Digit #%s within Fibonacci is" %n, n-1
-    else:
-        n = n -1
-        print "Digit #%s within Fibonacci is " %(n+1) + str(d(n))
-
-def d(n):
-    if n <= 1:
-        return n
-    else:
-        return (d(n-1) + d(n-2))
-
-## Tests whether or not f1(n) and f2(n) are equivalent
-
-def qa(n):
-    assert type(n) == int and n >  0
-    if f1(n) == f2(n):
-        print True
-    else:
-        print False
+#print(fib2(100))
